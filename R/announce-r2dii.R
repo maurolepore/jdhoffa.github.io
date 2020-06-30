8c68a8f5bddbb7a92d0bf55b2e3aa777137fca1b
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-06-30 13:39:13 +0200

    add rmarkdown for genreating posts with R code

---
+++ R/announce-r2dii.R
@@ -0,0 +1,21 @@
+library(jekyllr)
+library(here)
+library(knitr)
+
+blog_root = here::here()
+
+options(
+  jekyll.root = blog_root,
+  jekyll.default.template = "posts"
+)
+
+title = "r2dii suite is now on CRAN"
+
+categories = c("blog")
+
+tags = c("2 degrees investing initiative", "climate")
+
+jekyll.post('R/announce-r2dii.Rmd',
+             title,
+             categories,
+             tags)
